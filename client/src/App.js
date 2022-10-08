6b6039d6bfd432a9fe91cc745ccb506aa94e354d
Blackflame6 <engineerfranklyn@gmail.com>
2022-10-08 18:49:40 -0500

    created 6 components pages for the 6 navlinks in the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
-import "./App.css";
 import SharedLayout from "./components/SharedLayout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import About from "./pages/About";
+import Products from "./pages/Products"
+import Contact from "./pages/Contact"
+import Cart from "./pages/Cart"
+import Login from "./pages/Login"
+import Index from "./pages/Index";
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route>
-            <Route path="/" element={<SharedLayout />}/>
-            <Route path="/" element={<SharedLayout />}/>
-            <Route path="/" element={<SharedLayout />}/>
-            <Route path="/" element={<SharedLayout />}/>
-            <Route path="/" element={<SharedLayout />}/>
-            <Route path="/" element={<SharedLayout />}/>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Index />}/>
+            <Route index element={<About />}/>
+            <Route index element={<Products />}/>
+            <Route index element={<Contact />}/>
+            <Route index element={<Cart />}/>
+            <Route index element={<Login />}/>
+           
           </Route>
         </Routes>
       </div>
