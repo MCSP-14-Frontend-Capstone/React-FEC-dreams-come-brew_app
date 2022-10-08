14f2f81ac83b0d00e25888a4631fb6174bd2574f
Blackflame6 <engineerfranklyn@gmail.com>
2022-10-08 18:27:57 -0500

    added imported react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
+import "./App.css";
+import SharedLayout from "./components/SharedLayout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-
-    <div>Test</div>
-
+    <Router>
+      <div className="App">
+        <Routes></Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom"
+
+const NavBar = () => {
+  return ( 
+    <nav className="navbar">
+      <div>
+        Dreams Come Brew
+      </div>
+      {/* <div className="navlinks">
+        <NavLink>
+          About
+        </NavLink>
+        <NavLink>
+          Products
+        </NavLink>
+        <NavLink>
+        Contact 
+        </NavLink>
+        <NavLink>
+          Cart
+        </NavLink>
+      </div>
+
+      <NavLink>
+        Login
+      </NavLink> */}
+    </nav>
+  
+  )
+}
+
+export default NavBar
