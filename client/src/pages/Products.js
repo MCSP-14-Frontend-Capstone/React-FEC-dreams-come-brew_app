3cf4ffd8de50922b671d4570df94f1dec0208cc7
Blackflame6 <engineerfranklyn@gmail.com>
2022-10-08 20:01:41 -0500

    mapped over data objects tp display the products on the page

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import products from "../data";
+import { Link } from "react-router-dom";
 
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <>
+      <h2>Items are selling fast, hurry!</h2>
 
-export default Products
+      <div className="products">
+        {products.map((item) => {
+          return (
+            <article key={item.id} id={item.id}>
+              <h3>{item.name}</h3>
+              <img className="item-image" src={item.image} alt="" />
+              <div>
+                <p className="price-description">Price: ${item.price}</p>
+                <Link to={`/products/${item.id}`}>more info</Link>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Products;
