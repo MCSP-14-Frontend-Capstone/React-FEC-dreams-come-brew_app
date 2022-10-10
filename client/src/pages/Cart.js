08d3c16c88d9153b3cf34685c7ab47bd92b15920
Justin Alvarez <justinalvarez.1221@gmail.com>
2022-10-10 11:49:36 -0500

    JA cart layout

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <>
+    <div className = "ShoppingCart">Shopping Cart
+    </div><>QTY:
+      <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+    </>
+      <button>Delete</button>
+      <button>CheckOut</button>
+    </>
   )
 }
 
