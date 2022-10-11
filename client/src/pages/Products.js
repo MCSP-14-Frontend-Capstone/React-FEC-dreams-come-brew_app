432a5b91f683288f0af3657e49b9afa970ba7d2e
Blackflame6 <engineerfranklyn@gmail.com>
2022-10-11 13:34:40 -0500

    changed product page layout

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -26,39 +26,48 @@ const Products = () => {
                 key={product.id}
                 id={product.id}
               >
-                <h3 className="product-name">{product.name}</h3>
-                <Link className="single-link" to={`/products/${product.id}`}>
+                  <h3 className="product-name">{product.name}</h3>
+                <div className="df">
+                  <Link className="single-link" to={`/products/${product.id}`}>
                   
-                <img className="item-image" src={product.image} alt="" />
-                  </Link>
-               
-                <div>
-                  <p className="qty">Qty: {product.qty} +</p>
-                    <FontAwesomeIcon
-                      className="rating-icon"
-                      icon={faStar}
-                    ></FontAwesomeIcon>
-                    <FontAwesomeIcon
-                      className="rating-icon"
-                      icon={faStar}
-                    ></FontAwesomeIcon>
-                    <FontAwesomeIcon
-                      className="rating-icon"
-                      icon={faStar}
-                    ></FontAwesomeIcon>
-                    <FontAwesomeIcon
-                      className="rating-icon"
-                      icon={faStar}
-                    ></FontAwesomeIcon>
-                    <FontAwesomeIcon
-                      className="rating-icon"
-                      icon={faStarHalf}
-                    ></FontAwesomeIcon>
+                  <img className="item-image" src={product.image} alt="" />
+                    </Link>
+                  <div>
+                  <button className="btn add-to-cart-btn">Add to Cart <FontAwesomeIcon
+                        className="cart-icon"
+                        icon={faCartShopping}
+                      ></FontAwesomeIcon></button>
+                    <p className="qty">Qty: {product.qty} +</p>
+                      <FontAwesomeIcon
+                        className="rating-icon"
+                        icon={faStar}
+                      ></FontAwesomeIcon>
+                      <FontAwesomeIcon
+                        className="rating-icon"
+                        icon={faStar}
+                      ></FontAwesomeIcon>
+                      <FontAwesomeIcon
+                        className="rating-icon"
+                        icon={faStar}
+                      ></FontAwesomeIcon>
+                      <FontAwesomeIcon
+                        className="rating-icon"
+                        icon={faStar}
+                      ></FontAwesomeIcon>
+                      <FontAwesomeIcon
+                        className="rating-icon"
+                        icon={faStarHalf}
+                      ></FontAwesomeIcon>
                   
-                  <div className="price-info">
-             
+                    <div className="price-info">
+                  </div>
+                    <p className="price">${product.price} <p><sup>99</sup></p> </p>
+                    <hr className="line"/>
+                    <p>{product.description} <Link to={`/products/${product.id}`}>see more</Link> </p>
+                    <hr className="line"/>
+                    
                 </div>
-                  <p className="price">${product.price} <p><sup>99</sup></p> </p>
+                
                 
                 </div>
               </article>
