449e4e1e68238260ff47fba3276189aff54e6185
MarcoDayz <diaz.marco8686@gmail.com>
2022-10-11 12:13:39 -0700

    updated homepage deals component

--- client/src/components/DealsCompo.js
+++ client/src/components/DealsCompo.js
@@ -6,9 +6,11 @@ console.log(items)
     //map through items
     return (
         <div className="Deals">
+            <a href="#" className="prev">&#8249;</a>
             {items.map((item,i) => (
                 <DealsItem item={item} key={i}/>
             ))}
+            <a href="#" className="next">&#8250;</a>
         </div>
     )
 }
--- client/src/components/DealsItems.js
+++ client/src/components/DealsItems.js
@@ -2,12 +2,12 @@ const DealsItem = ({item}) => {
 
     return (
         <div className="DealItem">
+
             <div>
                 <img className ="DealsImg" src={item.image}></img>
             </div>
-            <h1>Item: {item.name} </h1>
+            <h1 className="DealItemName">Item: {item.name} </h1>
             <p>Price: {item.price}</p>
-            
         </div>
     )
 }
