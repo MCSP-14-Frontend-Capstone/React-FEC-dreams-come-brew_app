a220b6c464afc209714e8c0794b401545d5c1a5f
abelardom3 <abelardom1999@gmail.com>
2022-10-11 21:33:35 -0400

    added cart

--- client/src/pages/Cart/Cart.js
+++ client/src/pages/Cart/Cart.js
@@ -5,7 +5,6 @@ import Items from './Items'
 import CartCheckout from './CartCheckout'
 
 
-
 const arr = [
     { id: 1, name: 'test', price: 12.20, quantity: 2, total: 20, image: 'https://images.unsplash.com/photo-1562113530-57ba467cea38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80' },
     { id: 2, name: 'cake', price: 10.10, quantity: 1, total: 10, image: 'https://images.unsplash.com/photo-1602143407151-7111542de6e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80' },
