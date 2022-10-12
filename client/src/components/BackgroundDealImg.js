6632c88806b86b1f46ec2544a7ca46521e29bfc8
MarcoDayz <diaz.marco8686@gmail.com>
2022-10-12 11:46:26 -0700

    working homepage background image compo

--- client/src/components/BackgroundDealImg.js
+++ client/src/components/BackgroundDealImg.js
@@ -2,7 +2,11 @@ const BackgroundDealImg = ({item}) => {
 
     return (
         <div className="backgroundDeal-div">
-            <img className="backgroundDealimg" src={item.image}></img>
+            {/* <div> */}
+               <img className="backgroundDealimg" src={item.image}></img> 
+               <div className="gradienttest">test</div>
+            {/* </div> */}
+            
         </div>
     )
 }
