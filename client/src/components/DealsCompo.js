c2d4aadc287116114df3bc534b4dc7593c416405
MarcoDayz <diaz.marco8686@gmail.com>
2022-10-13 15:54:15 -0700

    updated home

--- client/src/components/DealsCompo.js
+++ client/src/components/DealsCompo.js
@@ -1,20 +1,20 @@
 import DealsItem from "./DealsItems";
-import {Link} from 'react-scroll';
+// import {Link} from 'react-scroll';
 
 const DealsCompo = ({items}) => {
 
     //map through items
     return (
         <div className="DealsMain">
-            {/* <div>This is Deals Component</div> */}
-        <Link className="DealsArrow" to="Deals" activeClass="active" duration={650} ignoreCancelEvents={false} offset={-700} smooth={true} horizontal={true} containerId="deals">&#8249;</Link>
+            <h2>Popular items</h2>
+        {/* <Link className="DealsArrow" to="Deals" activeClass="active" duration={650} ignoreCancelEvents={false} offset={-700} smooth={true} horizontal={true} containerId="deals">&#8249;</Link> */}
      
             <div className="Deals" id="deals">
             {items.map((item,i) => (
                 <DealsItem item={item} key={i}/>
             ))}
             </div>
-        <Link className="DealsArrow" to="Deals" activeClass="active" duration={650} ignoreCancelEvents={false} offset={2000} smooth ={true} horizontal={true} containerId="deals">&#8250;</Link>
+        {/* <Link className="DealsArrow" to="Deals" activeClass="active" duration={650} ignoreCancelEvents={false} offset={2000} smooth ={true} horizontal={true} containerId="deals">&#8250;</Link> */}
         </div>
        
     )
