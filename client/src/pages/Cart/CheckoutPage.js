ac47598643b5c4ff10b38965cb733bed1a938f6b
Justin Alvarez <justinalvarez.1221@gmail.com>
2022-10-13 09:27:48 -0500

    checkout messege

--- client/src/pages/Cart/CheckoutPage.js
+++ client/src/pages/Cart/CheckoutPage.js
@@ -4,10 +4,10 @@ const CheckoutPage = () =>{
     return(
         <div>
              <div>YOUR GOOD TO GO 
-                <h1>Thank You For Your Purchase</h1>
+                <h1 className='Checkout-messege'>Thank You For Your Purchase</h1>
             </div>
             <Link to ='/'>
-            <button>Back To Shopping</button>
+            <button className='BackButton'>Back To Shopping</button>
             </Link>
         </div>
     )
