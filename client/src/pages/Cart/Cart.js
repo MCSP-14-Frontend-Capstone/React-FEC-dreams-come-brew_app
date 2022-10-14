dbc7df3e9352b7efce226886ae3a0cca7f96f295
abelardom3 <abelardom1999@gmail.com>
2022-10-14 14:06:01 -0400

    added total

--- client/src/pages/Cart/Cart.js
+++ client/src/pages/Cart/Cart.js
@@ -1,9 +1,6 @@
 import React, { useState, useContext, } from 'react'
-import CartHeader from './CartHeader'
-import CartColumns from './CartColumn'
 import Items from './Items'
 import CartContext from '../../context/CartContext'
-import { Link } from "react-router-dom";
 import CartRec from './CartRec'
 import Summary from './Summary';
 
@@ -11,7 +8,7 @@ import Summary from './Summary';
 
 const Cart = () => {
     const { cart } = useContext(CartContext)
-   
+
     if (cart.length === 0) {
         return (
             <div className='main-cart-page'>
@@ -30,8 +27,7 @@ const Cart = () => {
             <div className='main-cart-page'>
                 <div className='cart-table'>
                     <div className='cart-header-box'>
-                        <CartHeader />
-                        <CartColumns />
+                        <h1 className='cart-header'>Ready To Checkout?</h1>
                     </div>
                 </div>
                 <div className='items-page'>
@@ -52,4 +48,3 @@ const Cart = () => {
     }
 }
 export default Cart
-/* <CartCheckout /> */
