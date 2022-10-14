79915ac56548bcf1ff5fd426614def2b5be12599
Blackflame6 <engineerfranklyn@gmail.com>
2022-10-13 23:47:50 -0500

    product page header

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,18 +1,22 @@
 const SearchBar = () => {
   return (
     <div className="search-container">
-      <div className="beside-search"></div>
-      <form action="">
-        \
-        <input className="search" type="text" />
-        <button type="submit">
-          {" "}
-          <img
-            src="https://www.seekpng.com/png/detail/65-659285_search-icon-transparent-background-material-design-magnifying-glass.png"
-            alt=""
-          />
-        </button>
-      </form>
+      <div className="beside-search">
+        <h2 className="beside-text">NEW DEALS ALL MONTH!!!</h2>
+      </div>
+      <div className="main-search">
+        <form className="searchbar" action="">
+          
+          <input className="search" type="text" placeholder="Search Anything" />
+          <button type="submit">
+            {" "}
+            <img
+              src="https://www.seekpng.com/png/detail/65-659285_search-icon-transparent-background-material-design-magnifying-glass.png"
+              alt=""
+            />
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
