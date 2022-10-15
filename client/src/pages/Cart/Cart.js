17df69c739a1f41c2183d821838a820fab87eafe
abelardom3 <abelardom1999@gmail.com>
2022-10-15 18:06:49 -0400

    edited cart

--- client/src/pages/Cart/Cart.js
+++ client/src/pages/Cart/Cart.js
@@ -14,10 +14,8 @@ const Cart = () => {
             <>
                 <div className='empty-main'>
                     <div className='empty-div'>
-
                         <h2 className='cart-empty-text'>Your cart is empty</h2>
-
-
+                        <img className='cart-img-empty' src='https://static.vecteezy.com/system/resources/previews/000/585/684/original/coffee-cup-logo-template-vector-icon-design.jpg' alt=''></img>
                     </div>
 
                 </div>
