4c72369f415ee358c71e6d7c6b6699442bc7a97b
abelardom3 <abelardom1999@gmail.com>
2022-10-16 18:22:02 -0400

    edited error pg

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>
-      <h2>ERROR</h2>
+    <div className="error-box">
+      <h2 className="error-text">404 Page Not Found</h2>
+      <img className='cart-img-empty' style={{ margin: 'auto' }} src='https://static.vecteezy.com/system/resources/previews/000/585/684/original/coffee-cup-logo-template-vector-icon-design.jpg' alt=''></img>
       <Link to="/">
-        <button> Back Home</button>
+        <button className="error-btn">Back To Homepage</button>
       </Link>
     </div>
   );
