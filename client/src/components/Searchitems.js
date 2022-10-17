edf5a74a4928c7b2bc27d0a1ec0f699ecc6733f2
Blackflame6 <engineerfranklyn@gmail.com>
2022-10-16 20:51:09 -0500

    fixed search bar bug

--- client/src/components/Searchitems.js
+++ client/src/components/Searchitems.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import products from '../data'
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import SearchContext from '../context/SearchContext'
 
 const Searchitems = ({product}) => {
+  const { searchValue, setSearchValue } = useContext(SearchContext);
+const handleClick = () => {
+ setSearchValue("")
+}
   return (
     <div className='hide2'>
                 <Link to={`/products/${product.target}`}>
-                  <li key={product.id}> {product.name}</li>
+                  <li onClick={handleClick} key={product.id}> {product.name}</li>
                 </Link>
     </div>
   )
