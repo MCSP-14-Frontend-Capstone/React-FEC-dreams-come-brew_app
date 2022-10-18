66f7c8ec7d76ee613d0ca735c7b287b7f15c391d
Brandon Martin <brandonmartin@Brandons-MacBook-Pro.local>
2022-10-18 16:13:02 -0600

    server Test routes

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const pool = require("./Routes/pool")
+const routes = require('./Routes/routes')
+
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3500;
+
+
+
+
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/products', routes.getAllProducts)
+
+app.get('/products/:id', routes.getOneProduct)
+
+app.post('/purchase', routes.addToCart)
+
+app.patch('/product/edit/:id', routes.editQuantity)
+
+app.delete('/product/remove', routes.deleteCart)
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
