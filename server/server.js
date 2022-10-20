53abaddf86a9efd04a6c7e5d3d5d76df919689a8
Blackflame6 <engineerfranklyn@gmail.com>
2022-10-20 11:48:06 -0500

    fixed environment variable

--- server/server.js
+++ server/server.js
@@ -1,47 +1,41 @@
 const contact = require('./Backend/Routes/Contact')
-
-const pool = require("../server/Backend/pool")
 const users = require('../server/Backend/Routes/users')
-
-// require('dotenv').config()
 
 
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3500;
 
 
+require('dotenv').config()
 app.use(express.json());
 app.use(cors());
 
-// //Products Routes
 
 
-// //User Routes
+// Contact Routes
+
+app.get('/contacts', contact.getAllContacts)
+
+app.get('/contacts/:id', contact.getOneContact)
+
+app.post('/contacts/post', contact.postContact)
+
+app.delete('/contacts/:id', contact.deleteContact)
 
 
+//User Routes
 
-// //Contact Routes
-// app.get('/contacts', contact.getAllContacts)
+app.get('/users', users.getAllUsers)
 
-// app.get('/contacts/:id', contact.getOneContact)
+app.get('/users/:id', users.getOneUser)
 
-// app.post('/contacts/post', contact.postContact)
+app.post('/users', users.addUser)
 
-// app.delete('/contacts/:id', contact.deleteContact)
+app.patch('/users/:id', users.editUser)
 
-
-
-// app.get('/users', users.getAllUsers)
-
-// app.get('/users/:id', users.getOneUser)
-
-// app.post('/users', users.addUser)
-
-// app.patch('/users/:id', users.editUser)
-
-// app.delete('/users/:id', users.deleteUser)
+app.delete('/users/:id', users.deleteUser)
 
 
 
