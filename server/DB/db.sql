584b4acec543611f6f3b409e7f8fd64c10c08f04
MarcoDayz <diaz.marco8686@gmail.com>
2022-10-20 15:29:36 -0700

    updated db sql

--- server/DB/db.sql
+++ server/DB/db.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS purchase;
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS contact;
 
-CREATE TABLE products(
+CREATE TABLE products (
     product_id SERIAL PRIMARY KEY,
     name TEXT,
     category TEXT,
@@ -15,7 +15,14 @@ CREATE TABLE products(
     image BYTEA
 );
 
-CREATE TABLE purchase(
+CREATE TABLE users (
+    users_id SERIAL PRIMARY KEY,
+    user_name TEXT,
+    password TEXT,
+    confirm_password TEXT
+);
+
+CREATE TABLE purchase (
     purchase_id SERIAL PRIMARY KEY,
     user_name TEXT,
     product_name TEXT, 
@@ -23,17 +30,15 @@ CREATE TABLE purchase(
     order_total MONEY,
     fees MONEY,
     taxes MONEY,
-    grand_total MONEY
+    grand_total MONEY,
+users_id SERIAL,
+ FOREIGN KEY(users_id) REFERENCES users(users_id),
+product_id SERIAL,
+    FOREIGN KEY(product_id) REFERENCES products (product_id)
 );
 
-CREATE TABLE users(
-    users_id SERIAL PRIMARY KEY,
-    user_name VARCHAR,
-    password VARCHAR,
-    confirm_password TEXT
-);
 
-CREATE TABLE contact(
+CREATE TABLE contact (
     contact_id SERIAL PRIMARY KEY, 
     names TEXT,
     email VARCHAR,
@@ -45,15 +50,17 @@ INSERT INTO products (name, category, description, original_price, sale_price, o
 INSERT INTO products (name, category, description, original_price, sale_price, on_sale, popular_item, image) VALUES('test3', 'test', 'test', 1, 1, False, True, null);
 INSERT INTO products (name, category, description, original_price, sale_price, on_sale, popular_item, image) VALUES('test4', 'test', 'test', 1, 1, False, True, null);
 
-INSERT INTO purchase (user_name,product_name,order_quantity,order_total,fees,taxes,grand_total) VALUES('test1', 'test', 1, 2,1,2,1);
-INSERT INTO purchase (user_name,product_name,order_quantity,order_total,fees,taxes,grand_total) VALUES('test2', 'test', 1, 2,1,2,1);
-INSERT INTO purchase (user_name,product_name,order_quantity,order_total,fees,taxes,grand_total) VALUES('test3', 'test', 1, 2,1,2,1);
-INSERT INTO purchase (user_name,product_name,order_quantity,order_total,fees,taxes,grand_total) VALUES('test4', 'test', 1, 2,1,2,1);
 
 INSERT INTO users (user_name,password,confirm_password) VALUES('username1','password', 'password');
-INSERT INTO users (user_name,password,confirm_password) VALUES('username2','password', 'password');
-INSERT INTO users (user_name,password,confirm_password) VALUES('username3','password', 'password');
-INSERT INTO users (user_name,password,confirm_password) VALUES('username4','password', 'password');
+INSERT INTO users (user_name,password,confirm_password) VALUES('username1','password', 'password');
+INSERT INTO users (user_name,password,confirm_password) VALUES('username1','password', 'password');
+INSERT INTO users (user_name,password,confirm_password) VALUES('username1','password', 'password');
+
+
+INSERT INTO purchase (user_name, product_name, order_quantity, order_total, fees, taxes, grand_total, users_id, product_id) VALUES('test1', 'test', 1, 2, 1, 2, 1, 1, 1);
+INSERT INTO purchase (user_name, product_name, order_quantity, order_total, fees, taxes, grand_total, users_id, product_id) VALUES('test1', 'test', 1, 2, 1, 2, 1, 2, 2);
+INSERT INTO purchase (user_name, product_name, order_quantity, order_total, fees, taxes, grand_total, users_id, product_id) VALUES('test1', 'test', 1, 2, 1, 2, 1, 2, 2);
+
 
 INSERT INTO contact (names, email, description) VALUES('test1', 'test', 'test');
 INSERT INTO contact (names, email, description) VALUES('test2', 'test', 'test');
