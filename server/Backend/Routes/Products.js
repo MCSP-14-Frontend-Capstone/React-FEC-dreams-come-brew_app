83009de6a62692699e5e918c36fd11846a561e17
Justin Alvarez <justinalvarez.1221@gmail.com>
2022-10-20 11:21:21 -0500

    productsRoutes

--- server/Backend/Routes/Products.js
+++ server/Backend/Routes/Products.js
@@ -1,4 +1,4 @@
-const pool = require('./pool');
+const pool = require('../pool');
 
 const getAllProducts = async (req, res) => {
     try {
@@ -12,26 +12,29 @@ const getAllProducts = async (req, res) => {
 const getOneProduct = async (req, res) => {
     const {id} = req.params
     try {
-        res.status(200).send("Get one route")
+        let {rows} = await pool.query('SELECT * FROM products WHERE product_id = $1', [id])
+        res.status(200).send(rows)
     } catch (error) {
         console.error(error.message)
     }
 }
 
-const addToCart = async (req, res) => {
-    const {body} = req.body
+const addToProduct = async (req, res) => {
+    const {name,category,description,original_price,sale_price,on_sale,popular_item} = req.body
     try {
-        res.status(201).send(body)
+        let {rows} = await pool.query('INSERT INTO products (name,category, description,original_price,sale_price,on_sale,popular_item) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING *',[name,category,description,original_price,sale_price,on_sale,popular_item])
+        res.status(201).send(rows)
     } catch (error) {
         console.error(error.message)
     }
 }
 
 const editQuantity = async (req, res) => {
+    const {order_quantity} = req.body
     const {id} = req.params
-    const {body} = req.body
     try {
-        res.status(201).send('patch')
+        let {rows} = await pool.query('UPDATE purchase SET order_quantity = $1 WHERE purchase_id = $2 RETURNING *', [order_quantity, id])
+        res.status(201).send(rows)
     } catch (error) {
         console.error(error.message)
     }
@@ -40,10 +43,11 @@ const editQuantity = async (req, res) => {
 const deleteCart = async (req, res) => {
     const {id} = req.params
     try {
-        res.status(200).send('delete')
+        let {rows} = await pool.query('DELETE FROM purchase WHERE product_id =$1 RETURNING *', [id])
+        res.status(200).send(rows)
     } catch (error) {
         console.error(error.message)
     }
 }
 
-module.exports = { getAllProducts, getOneProduct, addToCart, editQuantity, deleteCart }
+module.exports = { getAllProducts, getOneProduct, addToProduct, editQuantity, deleteCart }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-const pool = require("./Routes/pool")
-const routes = require('./Routes/routes')
+// const pool = require("./Routes/pool")
+// const routes = require('./Routes/routes')
+const product = require('./Backend/Routes/Products')
 // require('dotenv').config()
 
 
 const express = require('express');
 const cors = require('cors');
+// const { default: products } = require("../client/src/data");
 const app = express();
 const PORT = process.env.PORT || 3500;
 
@@ -13,15 +15,15 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get('/products', routes.getAllProducts)
+app.get('/products', product.getAllProducts)
 
-app.get('/products/:id', routes.getOneProduct)
+app.get('/products/:id', product.getOneProduct)
 
-app.post('/purchase', routes.addToCart)
+app.post('/purchase', product.addToProduct)
 
-app.patch('/product/edit/:id', routes.editQuantity)
+app.patch('/product/edit/:id', product.editQuantity)
 
-app.delete('/product/remove', routes.deleteCart)
+app.delete('/product/remove', product.deleteCart)
 
 
 
