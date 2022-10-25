19441e7633464a81808e8b0caf6e371651ef8e85
unknown <melvinlanza16@gmail.com>
2022-10-25 16:41:24 -0700

    single product add to cart fixed

--- client/src/pages/Singleproduct.js
+++ client/src/pages/Singleproduct.js
@@ -1,22 +1,24 @@
 import React, { useContext } from "react";
 import { useParams, Link } from "react-router-dom";
-import products from "../data";
+//import products from "../data";
 import CartContext from "../context/CartContext";
 import RandomContext from "../context/RandomContext";
 import randomFacts from "../randomFacts";
 import SearchBar from "../components/SearchBar";
 const Singleproduct = () => {
   const { productTarget } = useParams();
-  const product = products.find((product) => product.target === productTarget);
-  const { name, image, price, detail, description, pic1, pic2, pic3, pic4 } =
-    product;
+  
+  // const { name, image, price, detail, description, pic1, pic2, pic3, pic4 } =
+  //   product;
   //useContext for the cartcontext
-  const { handleClick, cart, Addon } = useContext(CartContext);
+  const { handleClick, cart, Addon, products } = useContext(CartContext);
   const { random, setRandom } = useContext(RandomContext)
+  const product = products.find((product) => product.target === productTarget);
   //handleObj changes the useContext value to be added to the cart
   const handleObj = (e) => {
     const id = e.target.id;
-    const found = cart.find((elem) => elem.id === product.id)
+    console.log(id);
+    const found = cart.find((elem) => elem.id === product.product_id)
     if (found) {
       console.log(found)
       Addon(found)
@@ -35,27 +37,27 @@ const Singleproduct = () => {
     <>
       {/* <SearchBar /> */}
       <div className="space">
-        <div key={product.id} id={product.id} className="main-single">
-          <img className="single-image" src={image} alt="" />
+        <div key={product.id} id={product.product_id} className="main-single">
+          <img className="single-image" src={product.image} alt="" />
           <div className="single-beside">
-            <h3>{name}</h3>
-            <p>{description}</p>
-            <p>{detail}</p>
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <p>{product.detail}</p>
             <div className="helper-images">
-              <img src={pic1} alt="" />
-              <img src={pic2} alt="" />
-              <img src={pic3} alt="" />
-              <img src={pic4} alt="" />
+              <img src={product.image} alt="" />
+              <img src={product.image} alt="" />
+              <img src={product.image} alt="" />
+              <img src={product.image} alt="" />
             </div>
             <div className="single-btns">
               <Link to="/Cart">
-                <button className="single-add-to-cart" onClick={handleObj}>Add to Cart</button>
+                <button className="single-add-to-cart" onClick={handleObj}  id={product.product_id}>Add to Cart</button>
               </Link>
               <Link to="/hotItems">
                 <button className="single-back-to-products">Hot Items</button>
               </Link>
               <Link to="/products">
-                <button className="single-back-to-products">All Products</button>
+                <button className="single-back-to-products" >All Products</button>
               </Link>
             </div>
           </div>
